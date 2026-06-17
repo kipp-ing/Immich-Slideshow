1f91eb920c12af61dc5b63adc9de1bfc19235ff8
Jan Kipping <app@kipp.ing>
2026-06-17 14:59:13 +0200

    Initial Commit

---
+++ Immich Slideshow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Immich Slideshow
+//
+//  Created by Jan Kipping on 17.06.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Immich Slideshow/Immich_SlideshowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Immich_SlideshowApp.swift
+//  Immich Slideshow
+//
+//  Created by Jan Kipping on 17.06.26.
+//
+
+import SwiftUI
+
+@main
+struct Immich_SlideshowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
