471f0d17a0811dff1264e6ee806c61c115bd7b45
kipp-ing <account@kipp.ing>
2026-06-24 12:58:36 +0200

    Add Immich shared-link auth resolver

--- Packages/ImmichClient/Sources/ImmichClient/ServerConfig.swift
+++ Packages/ImmichClient/Sources/ImmichClient/ServerConfig.swift
@@ -2,10 +2,27 @@ import Foundation
 
 public struct ServerConfig: Sendable {
     public let baseURL: URL
-    public let apiKey: String
+    public let auth: Auth
+
+    public enum Auth: Sendable, Equatable {
+        case apiKey(String)
+        case shareKey(String)
+    }
+
+    public var apiKey: String {
+        guard case let .apiKey(apiKey) = auth else {
+            return ""
+        }
+        return apiKey
+    }
 
     public init(baseURL: URL, apiKey: String) {
         self.baseURL = baseURL
-        self.apiKey = apiKey
+        self.auth = .apiKey(apiKey)
+    }
+
+    public init(baseURL: URL, auth: Auth) {
+        self.baseURL = baseURL
+        self.auth = auth
     }
 }
